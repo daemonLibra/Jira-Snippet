116ea000b3782577a7740c217534dc6e46e175fe
daemonlibra <daemonlibra@protonmail.com>
2025-01-13 18:03:04 +0100

    add new ElementsByClassName Id due to breaking changes from jira

--- scripts/copybtn.js
+++ scripts/copybtn.js
@@ -84,12 +84,21 @@ function createButton(parent) {
 var observer = new MutationObserver(function (mutations, me) {
   var parent = document.getElementsByClassName('gn0msi-0 cqZBrb')[0] ??
                document.getElementsByClassName('_otyr1y44 _ca0q1y44 _u5f3idpf _n3td1y44 _19bvidpf _1e0c116y')[0] ??
-               document.getElementsByClassName('_otyr1b66 _1yt4swc3 _1e0c116y')[0];
+               document.getElementsByClassName('_otyr1b66 _1yt4swc3 _1e0c116y')[0] ??
+               document.getElementsByClassName('_otyr1b66 _19pk1b66 _ca0q1b66 _u5f3idpf _n3td1b66 _19bvidpf _1e0c116y')[0];
   if (parent) {
     if (!document.getElementById('CopyBtnJiraId')) {
       createButton(parent);
     }
+    else
+    {
+      console.error("No Element found!");
+    }
     return;
+  }
+  else
+  {
+    console.error("No Parent found!");
   }
 });
 
